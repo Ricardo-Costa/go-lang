cb4e979d7623d109ebdc7aefbbe024e33385bd3b
Ricardo Costa <ricardo18acs@gmail.com>
2021-08-07 09:19:43 -0300

    Changes

--- slice.go
+++ slice.go
@@ -14,4 +14,23 @@ func main() {
 	fmt.Println(slice, len(slice))
 	fmt.Println(slice, cap(slice))
 
+
+	sl := make([]int, 2, 3)
+	for i := 0; i < 20; i++ {
+		sl = append(sl, i)
+		fmt.Println(sl)
+		fmt.Println("Len :", len(sl), " Cap:", cap(sl))
+	}
+
+	// mantendo com ponteiro
+	// sl = slice
+	sl3 := slice
+	slice[0] = 995
+
+	// Obs.: Se capacidade do slice for alterada internamente o apontamento tambem será afetado
+
+	//fmt.Println(sl)
+	fmt.Println(sl3)
+	fmt.Println(slice)
+
 }
